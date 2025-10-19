eea7c736856f642f935d04709e34c3f898d87452
Clinton Biswas <ponti.clinton@gmail.com>
2025-10-20 01:01:25 +0600

    hero image product details and pool page

--- rongdhonu/settings.py
+++ rongdhonu/settings.py
@@ -7,7 +7,6 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
-
 # Application definition
 
 INSTALLED_APPS = [
